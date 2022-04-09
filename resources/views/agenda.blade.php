e4166ad7047a6502196b96d44a0445fed98386a7
StarLord <blackhunterdev01@gmail.com>
2022-04-09 19:45:39 -0300

    substituição das views principais e modificação do layout.main

---
+++ resources/views/agenda.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', 'Agenda')
+
+@section('content')
+
+<div class="container_agenda">
+    <div class="calendario">
+        <h2>calendario</h2>
+    </div>
+    <div class="mentorias_agendadas">
+        <h2>mentorias agendadas</h2>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/mensagens.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('')
+
+@section('content')
+    
+    <h1>mensagens</h1>
+    <a href="/home">voltar</a>
+@endsection
