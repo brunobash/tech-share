08f8f233daffee2f665e914f56c484cefca74575
Diego Apolinario <dieego.apolinario@gmail.com>
2022-04-10 04:16:09 -0300

    Update landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,73 +6,75 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
-
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@700&family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
   <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
   <title>Technical Share</title>
 </head>
 
 <body>
-  <div id="container">
-    <div class="navbar">
-
-      <img src="{{ asset('img/logo.png') }}" alt="Logo" class="logo">
-      <div class="logo-text">
-        <h4>TECHNICAL SHARE</h4>
-        <p>o seu futuro agora!</p>
-      </div>
-
-      <nav>
-        <ul id="menu-list">
-          <li><a href="#">Quem somos</a></li>
-          <li><a href="#">Quero ser mentor</a></li>
-          <li><a href="#">Quero ser mentorado</a></li>
-        </ul>
-      </nav>
-      <img src="{{ asset('img/menu-icon.png') }}" class="menu-icon" alt="icone-menu" onclick="toggleMenu()">
+  <header>
+    <div class="logo">
+      <img src="{{ asset('img/logo.png') }}" alt="Logo">
     </div>
-
-    <main class="hero-center">
-      <h2>Sozinho você vai mais rápido, mas juntos vamos mais longe!</h2>
-      <img src="{{ asset('img/hero-image.png') }}" alt="imagem-tech">
-    </main>
-
-    <div class="text-orange">
-      <p>A <span>Technical Share</span> é uma plataforma que <span>conecta profissionais</span> da área de tecnologia a estudantes para a <span>troca de experiências</span> por meio de mentorias.</p>
+    <div class="logo-text">
+      <h3>TECHNICAL SHARE</h3>
+      <p>o seu futuro agora!</p>
     </div>
-
-    <div class="card-info">
-      <div class="card-box">
-        <h3>O que é mentoria?</h3>
-        <p>É um processo prático de crescimento profissional em que um mentor ensina ou dá ajuda e conselhos a uma pessoa menos experiente</p>
-      </div>
-      <div class="card-box">
-        <h3>Objetivo da mentoria</h3>
-        <p>Possibilitar a troca de experiências entre profissionais que já atuam no mercado e estudantes que estão iniciando ou em transição de carreira</p>
-      </div>
-      <div class="card-box">
-        <h3>Como funciona?</h3>
-        <p>O mentorado agenda a mentoria conforme o tipo de assunto que deseja desenvolver e conforme a disponibilidade do mentor</p>
-      </div>
+    <nav>
+      <ul>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Quero Participar</a></li>
+        <span></span>
+        <li><a href="#">Login</a></li>
+      </ul>
+    </nav>
+    <div class="menu-toggle">
+      <img src="{{ asset('img/menu-icon.png') }}" alt="menu">
     </div>
+  </header>
 
-    <footer>
+  <main>
+    <h2>Sozinho você vai mais rápido, mas juntos vamos mais longe!</h2>
+    <img src="{{ asset('img/hero-image.png') }}" alt="imagem-tech" class="hero-image">
+  </main>
 
-    </footer>
+  <div class="text-orange">
+    <p>A <span>Technical Share</span> é uma plataforma que <span>conecta profissionais</span> da área de tecnologia a estudantes para a <span>troca de experiências</span> por meio de mentorias.</p>
   </div>
 
-  <script>
-    var menuList = document.getElementById("menu-list");
-    menuList.style.maxHeight = "0px";
+  <div class="card-info">
+    <div class="card-box">
+      <h3>O que é mentoria?</h3>
+      <p>É um processo prático de crescimento profissional em que um mentor ensina ou dá ajuda e conselhos a uma pessoa menos experiente</p>
+    </div>
+    <div class="card-box">
+      <h3>Objetivo</h3>
+      <p>Possibilitar a troca de experiências entre profissionais que já atuam no mercado e estudantes que estão iniciando ou em transição de carreira</p>
+    </div>
+    <div class="card-box">
+      <h3>Como funciona?</h3>
+      <p>O mentorado agenda a mentoria conforme o tipo de assunto que deseja desenvolver e conforme a disponibilidade do mentor</p>
+    </div>
+  </div>
 
-    function toggleMenu() {
-      if (menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "130px";
-      } else {
-        menuList.style.maxHeight = "0px";
-      }
-    }
+  <footer>
+    <div class="card-footer">
+      <img src="{{ asset('img/study.png') }}" alt="imagem estudando" class="image-footer">
+      <a href="#">QUERO PARTICIPAR</a>
+    </div>
+  </footer>
+  <div class="btn-up">
+    <img src="{{ asset('img/seta-para-cima.png') }}" alt="botão volta pro topo">
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('.menu-toggle').click(function() {
+        $('nav').toggleClass('active')
+      })
+    })
   </script>
 </body>
 
