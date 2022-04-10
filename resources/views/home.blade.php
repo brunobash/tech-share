738664c3a7e0d70473a7b85357a60296bb52b6e3
StarLord <blackhunterdev01@gmail.com>
2022-04-09 21:54:52 -0300

    card adicionado(não finalizado) e modificações na /home e /mensagens

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,89 @@
 
 @section('content')
 
-<a href="/mentorias/create">criar mentorias</a>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/home_card.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="box">
+            <div class="image">
+                <!-----<img src="img1.jpeg">------->
+            </div>
+            <div class="name">David Chrish</div>
+            <div class="name_job">Dev Backend</div>
+            <div class="rating">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <div class="skills">
+                <p>
+                    ReacJS, Vue.js, Node.js
+                </p>
+            </div>
+            <div class="btns">
+                <button>Read more</button>
+                <button>Ver agenda</button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <!------  <img src="img2.jpeg" alt="">--->
+            </div>
+            <div class="name">David Chrish</div>
+            <div class="name_job">Dev Backend</div>
+            <div class="rating">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <div class="skills">
+                <p>
+                    ReacJS, Vue.js, Node.js
+                </p>
+            </div>
+            <div class="btns">
+                <button>Read More </button>
+                <button>Ver agenda</button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <!---- <img src="img3.jpeg" alt="">---->
+            </div>
+            <div class="name">David Chrish</div>
+            <div class="name_job">Dev Backend</div>
+            <div class="rating">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <div class="skills">
+                <p>
+                    ReacJS, Vue.js, Node.js
+                </p>
+
+            </div>
+            <div class="btns">
+                <button>Read More</button>
+                <button>Ver agenda</button>
+            </div>
+        </div>
+    </div>
+    <div class="create_mentoria">
+        <a href="/mentoria/create">Criar Mentoria</a>
+    </div>
+</body>
 
 @endsection
-
--- resources/views/mensagens.blade.php
+++ resources/views/mensagens.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('')
+@section('title', 'Mensagens')
 
 @section('content')
     
